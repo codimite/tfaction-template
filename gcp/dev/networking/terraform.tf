a8ae054551f3d0925f6fdfcf9233cfe8fe1b13be
rochana-atapattu <31321610+rochana-atapattu@users.noreply.github.com>
2023-11-28 16:57:10 +0530

    Update terraform.tf

--- gcp/dev/networking/terraform.tf
+++ gcp/dev/networking/terraform.tf
@@ -14,6 +14,10 @@ terraform {
       source  = "hashicorp/google"
       version = "~> 5.0"
     }
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.0"
+    }
   }
   required_version = ">= 1.6"
 }
